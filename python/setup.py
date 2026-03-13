f9077898573bc0aa0e6b6f952fd825bff321c08d
Abhishek Dana <danaabhi7119@gmail.com>
2026-03-13 11:30:06 +0530

    chore(sdk): bump version to 1.0.1

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="antiscamai",
-    version="1.0.0",
+    version="1.0.1",
     description="AntiScam AI – AI-powered request inspection middleware for Python",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
